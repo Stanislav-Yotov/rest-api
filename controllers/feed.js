f513a87007aabe70b9d8909f2f6a3dbea336d9c1
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-06-05 15:03:19 +0300

    added error handling

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,11 +1,12 @@
 const { validationResult } = require('express-validator');
+const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
     res.status(200).json({
         posts: [{
             _id: '1',
-            title: 'Book post', 
-            content: 'This is content', 
+            title: 'Book post',
+            content: 'This is content',
             imageUrl: '/images/book.jpg',
             creator: {
                 name: 'Stancho'
@@ -18,24 +19,32 @@ exports.getPosts = (req, res, next) => {
 exports.createPost = (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        res.status(422).json({
-            message: 'Validation failed',
-            errors: errors.array()
-        });
+        const error = new Error('Validation failed');
+        error.statusCode = 422;
+        throw error;
     }
     const title = req.body.title;
     const content = req.body.content;
-
-    res.status(201).json({
-        message: 'Post created successfuly!',
-        post: {
-            _id: new Date().toISOString(),
-            title: title,
-            content: content,
-            creator: {
-                name: 'Stancho'
-            },
-            createdAt: new Date()
+    const post = new Post({
+        title: title,
+        content: content,
+        imageUrl: '/images/book.jpg',
+        creator: {
+            name: 'Stancho'
         }
-    })
+    });
+    post.save()
+        .then(result => {
+            console.log(result);
+            res.status(201).json({
+                message: 'Post created successfuly!',
+                post: result
+            })
+        })
+        .catch(err => {
+            if (err.statusCode) {
+                err.statusCode = 500;
+            }
+           next(err);
+        });
 };
