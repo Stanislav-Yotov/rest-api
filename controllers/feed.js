ff067c74a7f6849759d32e85786d1847eb4b961c
Stanislav Yotov <stanislav.iotov@abv.bg>
2022-06-06 15:43:59 +0300

    added delete post and pagination

--- controllers/feed.js
+++ controllers/feed.js
@@ -4,9 +4,22 @@ const fs = require('fs');
 const path = require('path');
 
 exports.getPosts = (req, res, next) => {
+    const currentPage = req.query.page || 1;
+    const perPage = 2;
+    let totalItems;
     Post.find()
+        .countDocuments()
+        .then(count => {
+            totalItems = count;
+            return Post.find()
+            .skip((currentPage - 1) * perPage)
+            .limit(perPage);
+        })
         .then(posts => {
-            res.status(200).json({ posts: posts })
+            res.status(200).json({
+                 posts: posts, 
+                 totalItems: totalItems 
+                })
         })
         .catch(err => {
             if (err.statusCode) {
@@ -116,6 +129,30 @@ exports.updatePost = (req, res, next) => {
             }
             next(err);
         })
+};
+
+exports.deletePost = (req, res, next) => {
+    const postId = req.params.postId;
+    Post.findById(postId)
+        .then(post => {
+            if (!post) {
+                const error = new Error('Could not find post');
+                error.statusCode = 404;
+                throw error;
+            }
+            clearImage(post.imageUrl)
+            return Post.findByIdAndRemove(postId);
+        })
+        .then(result => {
+            console.log(result);
+            res.status(200).json({ message: 'Post deleted' })
+        })
+        .catch(err => {
+            if (!err.statusCode) {
+                err.statusCode = 500;
+            }
+            next(err);
+        });
 };
 
 const clearImage = filePath => {
